9227ebdc9b5ad4bd2864fa1c4685f1c56738a3e2
ferva <ferchovargas24@gmail.com>
2017-10-11 12:57:56 -0500

    Primer push

---
+++ src/main/java/org/unitec/elementos2017/Elementos2017Application.java
@@ -0,0 +1,12 @@
+package org.unitec.elementos2017;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Elementos2017Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Elementos2017Application.class, args);
+	}
+}
