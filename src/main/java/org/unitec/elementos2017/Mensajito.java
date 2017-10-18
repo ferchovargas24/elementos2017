f134e8f9761a81d11f1d07bf7cf254b03064d304
ferva <ferchovargas24@gmail.com>
2017-10-17 19:08:40 -0500

    tercer commit

--- src/main/java/org/unitec/elementos2017/Mensajito.java
+++ src/main/java/org/unitec/elementos2017/Mensajito.java
@@ -1,4 +1,59 @@
 package org.unitec.elementos2017;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Mensajito {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String titulo;
+
+    private String cuerpo;
+
+    public Mensajito() {
+    }
+
+    public Mensajito(Integer id) {
+        this.id = id;
+    }
+
+    public Mensajito(String titulo, String cuerpo) {
+        this.titulo = titulo;
+        this.cuerpo = cuerpo;
+    }
+
+    public Mensajito(Integer id, String titulo, String cuerpo) {
+        this.id = id;
+        this.titulo = titulo;
+        this.cuerpo = cuerpo;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getCuerpo() {
+        return cuerpo;
+    }
+
+    public void setCuerpo(String cuerpo) {
+        this.cuerpo = cuerpo;
+    }
 }
---
+++ src/main/java/org/unitec/elementos2017/RepositorioMensajito.java
@@ -0,0 +1,9 @@
+package org.unitec.elementos2017;
+
+import java.io.Serializable;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RepositorioMensajito extends
+        CrudRepository<Mensajito, Integer>{
+
+}
